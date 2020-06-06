445c1cb307eac3cd8e3ee7f688241bf4767c781a
kdy41000 <kdy41000@naver.com>
2020-06-06 20:30:20 +0900

    connect database2

--- BOBO/src/main/java/com/springvue/prectice/dao/UserMapper.java
+++ BOBO/src/main/java/com/springvue/prectice/dao/UserMapper.java
@@ -2,12 +2,13 @@ package com.springvue.prectice.dao;
 
 import com.springvue.prectice.vo.UserDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 @Mapper
 public interface UserMapper {
-    List<UserDto>selectList();
+    @Select(" SELECT * FROM USER ")
+    List<UserDto> selectList();
 }
