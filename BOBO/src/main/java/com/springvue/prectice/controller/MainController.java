b2c592637d534b5671c426fa2f020e551d1a89ca
kdy41000 <kdy41000@naver.com>
2020-06-06 20:59:26 +0900

    connect database3

--- BOBO/src/main/java/com/springvue/prectice/controller/MainController.java
+++ BOBO/src/main/java/com/springvue/prectice/controller/MainController.java
@@ -1,7 +1,5 @@
 package com.springvue.prectice.controller;
 
-import com.springvue.prectice.service.UserService;
-import com.springvue.prectice.vo.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,15 +10,9 @@ import java.util.List;
 @Controller
 public class MainController {
 
-    @Autowired
-    UserService userservice;
-
     @GetMapping("/selectlist")
     public ResponseEntity<?> TestMethod() {
         System.out.println("전체조회 컨트롤러 호출");
-        List<UserDto>list = userservice.selectList();
-        System.out.println(list.toString());
-        System.out.println(list.size());
         return ResponseEntity.ok("");
     }
 
