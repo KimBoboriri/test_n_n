245412e7ba6478f638e20d34d9368cb04627a5d4
kdy41000 <kdy41000@naver.com>
2020-06-06 20:07:05 +0900

    connect database

--- BOBO/src/main/java/com/springvue/prectice/service/UserService.java
+++ BOBO/src/main/java/com/springvue/prectice/service/UserService.java
@@ -1,20 +1,11 @@
 package com.springvue.prectice.service;
 
-import com.springvue.prectice.dao.UserMapper;
 import com.springvue.prectice.vo.UserDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserServiceImpl  {
-
-    @Autowired
-    private UserMapper usermapper;
-
-    public List<UserDto> selectList(){
-        System.out.println("서비스단");
-        return usermapper.selectList();
-    }
+public interface UserService {
+    public List<UserDto> selectList();
 }
---
+++ BOBO/src/main/java/com/springvue/prectice/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.springvue.prectice.service;
+
+import com.springvue.prectice.dao.UserMapper;
+import com.springvue.prectice.vo.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserMapper usermapper;
+
+    @Override
+    public List<UserDto> selectList(){
+        System.out.println("서비스단");
+        return usermapper.selectList();
+    }
+}
